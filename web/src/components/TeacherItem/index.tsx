a614b67f31b8d127d9056ed28a056a2417de8b28
thabcm <thalitahelp@gmail.com>
2020-08-05 16:19:32 -0300

    commit aula 3 front e back connection

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,38 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+    }
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return (
         <article className="teacher-item" >
                 <header>
-                    <img src="https://image.freepik.com/fotos-gratis/positivo-feminino-sorrindo-engracado-modelo-posando-perto-de-parede-rosa-no-estudio_158538-3433.jpg" alt="Thalita Barros"/>
+                    <img src={teacher.avatar} alt={teacher.name}/>
                     <div>
-                        <strong>Thalita Barros</strong>
-                        <span>Química</span>
+                        <strong>{teacher.name}</strong>
+                        <span>{teacher.subject}</span>
                     </div>
                 </header>
 
-                <p>
-                Quando você encontra alguém especial e se apaixona por essa pessoa,
-                 você começa a construir um relacionamento com os cuidados de quem 
-                 constrói uma Maravilha. 
-                 <br></br>
-                 Seus materiais básicos são constituídos de 
-                 muito Amor, Companheirismo e Dedicação.
-                </p>
+                <p>{teacher.bio}</p>
 
                 <footer>
                     <p>
                         Preço/hora 
-                        <strong>R$ 60,00</strong>    
+                        <strong>R$ {teacher.cost}</strong>    
                     </p>
-                    <button type="button" >
+                    <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`} 
+                    >
                         <img src={whatsappIcon} alt="Whatsapp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
-            </article>
+        </article>
     )
 }
 
